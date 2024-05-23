08b52bab75d548a3f5833ea79afe2df06ee2177f
Naman8042 <131790382+Naman8042@users.noreply.github.com>
2024-05-23 23:00:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ cloudinary.cloudinaryConnect()
 
 app.use(cors({
     // origin: 'http://localhost:5173',
-    origin:'https://frontend-theta-eight-17.vercel.app/',
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }))
